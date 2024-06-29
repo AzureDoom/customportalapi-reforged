a535b20416b44c64d109f32670c4e4e4756230ac
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-06-29 10:45:46 -0400

    More Lint

--- src/main/java/net/kyrptonaught/customportalapi/client/CustomPortalParticle.java
+++ src/main/java/net/kyrptonaught/customportalapi/client/CustomPortalParticle.java
@@ -5,15 +5,12 @@ import net.kyrptonaught.customportalapi.util.ColorUtil;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
 import net.minecraft.core.particles.BlockParticleOption;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 
 public class CustomPortalParticle extends PortalParticle {
     protected CustomPortalParticle(ClientLevel clientWorld, double d, double e, double f, double g, double h, double i) {
         super(clientWorld, d, e, f, g, h, i);
     }
 
-    @OnlyIn(Dist.CLIENT)
     public static class Factory implements ParticleProvider<BlockParticleOption> {
         private final SpriteSet spriteProvider;
 
